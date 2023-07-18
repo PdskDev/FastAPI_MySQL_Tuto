eeda561c81317f1c54cf940b19c060a413837d05
PdskDev <prestadesk@gmail.com>
2023-07-18 12:55:25 +0200

    Adding Models

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE= ''
+URL_DATABASE= 'localhost:3306/fastapi_db'
 
 engine = create_engine(URL_DATABASE)
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
